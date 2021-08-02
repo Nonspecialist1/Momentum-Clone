d7aa91ce86ffb881aba4b189594f7607ede46fdc
Sims <85279013+Nonspecialist1@users.noreply.github.com>
2021-08-03 01:25:30 +0900

    Add files via upload
    second pre-finisth

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "3.jfif"];
+const images = ["0.jpeg", "1.jpg", "3.jfif"];
 const chosenImage =  images[Math.floor(Math.random() * images.length)];
 document.body.style.backgroundImage = `url(img/${chosenImage})`
 // const bgImage = document.createElement("img");
